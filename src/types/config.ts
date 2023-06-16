c95d8cd4e6c03327e09e7261c5b429eaf23d851b
Alexandru Corut <alexandru.corutz@gmail.com>
2023-06-16 16:22:40 +0300

    add GroupNames

--- src/types/config.ts
+++ src/types/config.ts
@@ -3,6 +3,16 @@ import { Tax } from './tax'
 export interface Payments {
     enabled: boolean
 }
+
+interface GroupNames {
+    name: string
+    nickname: string
+}
+
+export interface GroupConfig {
+    names: GroupNames[]
+}
+
 export interface Config {
     payments: Payments
     tax: Tax
