563755c58fa4ebb87d0ee03989b513a4905a0202
Alexandru Corut <alexandru.corutz@gmail.com>
2024-03-22 10:12:13 +0200

    Update tax.ts

--- src/types/tax.ts
+++ src/types/tax.ts
@@ -1,14 +1,14 @@
-export interface Company {
-    name: string
-    cif: string
-}
-
 export interface Tax {
     meal: number
     tuition: number
     total: number
     company: Company
     extras: Extra[]
+}
+
+export interface Company {
+    name: string
+    cif: string
 }
 
 export interface Extra {
