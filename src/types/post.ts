f8d2f945e47a7cedfdad71a7d0a8834628bf841b
Alexandru Corut <alexandru.corutz@gmail.com>
2024-04-11 14:30:34 +0300

    updates

--- src/types/post.ts
+++ src/types/post.ts
@@ -2,7 +2,7 @@ import { Attachment } from './attachment'
 import { Comment } from './comment'
 import { Metadata } from './metadata'
 import { Poll } from './poll'
-import { INITIAL_USER, User } from './user'
+import { User } from './user'
 
 export enum PostType {
     Post,
@@ -16,9 +16,9 @@ export interface Post extends Metadata {
     body: string
     attachments: Attachment[]
     reviewed: boolean
-    author: User
-    likedBy: User[]
-    bookmarkedBy: User[]
+    author: User | string
+    likedBy: User[] | string[]
+    bookmarkedBy: User[] | string[]
     comments: Comment[]
     commentsOn: boolean
     liveAt: string | null
@@ -37,7 +37,7 @@ export const INITIAL_POST: Post = {
     body: '',
     attachments: [],
     reviewed: false,
-    author: INITIAL_USER,
+    author: '',
     likedBy: [],
     bookmarkedBy: [],
     comments: [],
